5771845a13748bc00fcec05a51bd5fddaf9e5332
Etienne Boursault <43215408+EhBoss@users.noreply.github.com>
2022-07-23 21:02:18 +0200

    Initial commit

---
+++ src/AgeLessThanZeroException.java
@@ -0,0 +1,15 @@
+public class AgeLessThanZeroException extends IllegalArgumentException{
+
+    public AgeLessThanZeroException(){}
+    public AgeLessThanZeroException(String message) {
+        super(message);
+    }
+
+    public AgeLessThanZeroException(Throwable cause) {
+        super(cause);
+    }
+
+    public AgeLessThanZeroException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/CustomExceptionTutorial.java
@@ -0,0 +1,13 @@
+public class CustomExceptionTutorial {
+    public static void main(String[] args) throws AgeLessThanZeroException{
+
+        validateAge(-3);
+    }
+
+    private static void validateAge(int age) throws AgeLessThanZeroException {
+        if (age < 0) {
+            throw new AgeLessThanZeroException("Age cannot be negative", new RuntimeException());
+        }
+
+    }
+}
